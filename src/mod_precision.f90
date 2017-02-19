7c806e511caaa0c785c659b10a3135dabdecb838
Sylvain LANNUZEL <sylvain@dhcp-147-251.via.ecp.fr>
2017-02-19 15:55:14 +0100

    heat equation

--- src/mod_precision.f90
+++
@@ -1,3 +0,0 @@
-module mod_precision
-  integer, parameter :: dp = kind(1.d0)
-end module mod_precision
--- src/rho.f90
+++
@@ -1,5 +0,0 @@
-double precision function rho(n, t, y)
-  integer n
-  double precision :: t
-  double precision, dimension(n)  :: y
-end function rho
